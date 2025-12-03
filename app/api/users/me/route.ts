8a3a0e3841461fc098293e3b455e4ef300403180
Souvik Ghosh <mesouvikghosh96@gmail.com>
2025-12-03 22:11:17 +0530

    add helper function and logout, me routes

---
+++ app/api/users/me/route.ts
@@ -0,0 +1,22 @@
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/userModel";
+import { connect } from "@/dbConfig/dbConfig";
+
+
+export async function GET(request:NextRequest){
+    await connect();
+
+    try {
+        const userId = await getDataFromToken(request);
+        const user = await User.findOne({_id: userId}).select("-password").select("-_id").select("-lastLogin");
+        return NextResponse.json({
+            message: "User found",
+            success: true,
+            user: user
+        })
+    } catch (error:any) {
+        return NextResponse.json({error: error.message}, {status: 400});
+    }
+}
